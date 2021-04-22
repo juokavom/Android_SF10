d998d1938497e820f446ba0fa7f68befdfc0862a
Jokubas Akramas <j.akramas@gmail.com>
2021-04-22 15:03:21 +0300

    Main window drawer UI

--- app/src/main/java/com/sf10/android/activities/MainActivity.kt
+++ app/src/main/java/com/sf10/android/activities/MainActivity.kt
@@ -11,26 +11,44 @@ import com.sf10.android.firebase.FirestoreClass
 import com.sf10.android.models.User
 
 class MainActivity : BaseActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private var mUser: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fetchUser({ user ->
+            mUser = user
+        }, {
+            Log.d("User", "Fully parsed user object = $mUser")
+        })
+
+
+//        binding.logout.setOnClickListener {
+//
+//        }
+    }
+
+
+    private fun fetchUser(setUser: (User) -> Unit, callback: () -> Unit) {
         FirestoreClass().getUser({ user ->
-            Log.d("User", "object = $user")
-            binding.uid.text = user.id
-            binding.email.text = user.email
-            binding.username.text = user.username
+            Log.d("User", "user object = $user")
+            setUser(user)
+            callback()
+//            binding.uid.text = user.id
+//            binding.email.text = user.email
+//            binding.username.text = user.username
         }, {
             Log.d("User", "does not exist")
         })
+    }
 
-        binding.logout.setOnClickListener {
-            AuthUI.getInstance().signOut(this)
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }
+    private fun logout() {
+        AuthUI.getInstance().signOut(this)
+        startActivity(Intent(this, IntroActivity::class.java))
+        finish()
     }
 }
